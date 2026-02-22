ae5894b1d2dee2044bc3661a83a6fbeb350819dd
sairampv5 <ramsaipv5@gmail.com>
2026-02-22 16:34:54 +0530

    day7

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const mysql = require('./config/db_mysql');
 const connectMongo = require('./config/db_mongo');
+const router = require('./routes/customer_logic');
+const mCrud = require('./routes/mongoCrud');
+const pRouter = require('./routes/product');
+const sRouter = require('./routes/sell');
 require('dotenv').config();
 
 const app = express();
 connectMongo();
+app.use(express.json());
+app.use('/products', pRouter);
+app.use('/api', router);
+app.use('/mongo-crud', mCrud);
+app.use('/', sRouter);
 
 app.get('/test', async (req,res) => {
     try {
-        const[rows] = await mysql.query("SELECT 'MySQL is Ready' AS stat");
+        const[rows] = await mysql.query("SELECT 'MySQL is Ready' AS stat ");
         /*const[row1] = await mysql.query("SELECT 'Checking if this query will print' AS test");
         , mysql:row1[1].test*/
         res.json({
